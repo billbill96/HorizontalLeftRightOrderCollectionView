8dfb2ef577243062c79677b8e5c5a2d421d15dda
Supannee Mutitanon <supannee.muu@gmail.com>
2023-01-17 17:01:20 +0700

    Initial Commit

---
+++ HorizontalCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HorizontalCollectionView
+//
+//  Created by Supannee Mutitanon on 17/1/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
